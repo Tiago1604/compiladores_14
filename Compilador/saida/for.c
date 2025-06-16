81b0a5da1f2db7e7a97a0bf1e99ed8a85948e7b6
isacostaf <iisabellecostaf@gmail.com>
2025-06-16 19:17:09 -0300

    erro for -int x

---
+++ Compilador/saida/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int i;
+int soma;
+    soma = 0;
+    for (int i = 0; i < 5; i++) {
+        soma = (soma + i);
+        printf("%d\n", soma);
+    }
+    return 0;
+}
