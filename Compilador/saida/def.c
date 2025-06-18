bfa87e5077596e99f2c5c2962f8be485b3e287b1
johnaopedro <johnaopedro@gmail.com>
2025-06-18 15:06:54 -0300

    feat:removendo arquivo sem funcao

--- Compilador/saida/def.c
+++ Compilador/saida/def.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+void minhaFuncao() {
+    int x;
+    x = 10;
+    printf("%d\n", x);
+}
+
 int main() {
-void minhaFuncao(){
-        x = 10;
-        printf("%d\n", x);
-    }
     return 0;
 }
